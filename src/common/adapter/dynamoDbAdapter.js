9e96b54a76944c069e0161af1c6e27940986c563
Rocamain <fjrocavazquez@gmail.com>
2023-04-28 17:49:30 +0100

    creates delete and batchDelete and modify rest function according to changes

--- src/common/adapter/dynamoDbAdapter.js
+++ src/common/adapter/dynamoDbAdapter.js
@@ -4,6 +4,8 @@ const {
   GetCommand,
   PutCommand,
   QueryCommand,
+  DeleteCommand,
+  BatchWriteCommand,
 } = require('@aws-sdk/lib-dynamodb')
 const { NodeHttpHandler } = require('@aws-sdk/node-http-handler')
 const https = require('https')
@@ -56,6 +58,7 @@ module.exports = class DynamoDbAdapter {
 
   async createItem(tableName, entity) {
     console.log(`Saving new item into DynamoDB table ${tableName}`)
+    console.log('Entty', entity)
 
     const params = {
       Item: entity,
@@ -65,7 +68,7 @@ module.exports = class DynamoDbAdapter {
     }
 
     await this.create(params)
-    console.log('Item saved successfully', entity)
+    console.log('Item saved successfully')
     return entity
   }
 
@@ -74,27 +77,33 @@ module.exports = class DynamoDbAdapter {
       ...params,
     })
 
-    return this.documentClient.send(command)
+    const response = await this.documentClient.send(command)
+
+    return response
   }
 
-  async getItem(tableName, id) {
+  async getItem(tableName, { id, userId }) {
     console.log(`Getting item id=${id} from DynamoDB table: ${tableName}`)
     const params = {
       TableName: tableName,
-      Key: { PK: id },
-      // ConsistentRead: true,
-    }
-    if (!id || id.trim() === '') {
-      const error = new Error()
-      error.msg = 'Id required'
-      error.statusCode = 400
+      Key: {
+        PK: id,
+        userId,
+      },
 
-      throw JSON.parse(error)
+      ConsistentRead: true,
     }
+    // if (!id || id.trim() === '') {
+    //   const error = new Error()
+    //   error.msg = 'Id required'
+    //   error.statusCode = 400
 
-    const entity = await this.get(params)
+    //   throw JSON.parse(error)
+    // }
+
+    const response = await this.get(params)
     console.log('Item retrieved successfully')
-    return entity
+    return response.Item
   }
 
   async get(params) {
@@ -102,6 +111,100 @@ module.exports = class DynamoDbAdapter {
       ...params,
     })
     return this.documentClient.send(command)
+  }
+
+  async deleteItem(tableName, { id, userId }) {
+    console.log(`Getting item id=${id} from DynamoDB table: ${tableName}`)
+
+    const params = {
+      TableName: tableName,
+      Key: { PK: id, userId },
+      ReturnConsumedCapacity: 'TOTAL',
+    }
+
+    const response = await this.delete(params)
+    console.log('Item deleted successfully')
+    return response
+  }
+
+  async deleteItems(tableName, { id, userId, items, count }) {
+    console.log(
+      `Deleting items with id=${id} from DynamoDB table: ${tableName}`,
+    )
+    if (count === 1) {
+      const response = await this.deleteItem(tableName, { id, userId })
+      return response
+    } else {
+      const itemKeys = items.map((item) => ({
+        PK: id,
+        userId: item.userId,
+      }))
+
+      // Delete the items in batches of 25 (the maximum allowed by DynamoDB)
+
+      const batchSize = 25
+
+      //  recursive function to delete on batches of 25.
+
+      const recursiveBatchDelete = async (itemKeys, count, batchSize) => {
+        if (count <= 0) {
+          return
+        }
+        const batchItems = {
+          [tableName]: itemKeys.slice(0, 25).map((key) => ({
+            DeleteRequest: { Key: key },
+          })),
+        }
+        const batchWriteCommand = {
+          RequestItems: batchItems,
+          ReturnConsumedCapacity: 'TOTAL',
+        }
+
+        await this.batchDelete(batchWriteCommand)
+        console.log('Batch Items deleted successfully')
+
+        return recursiveBatchDelete(itemKeys, count - 25, batchSize)
+      }
+
+      return await recursiveBatchDelete(itemKeys, count, batchSize)
+    }
+  }
+
+  async delete(params) {
+    const command = new DeleteCommand({
+      ...params,
+    })
+    console.log(command)
+    const response = await this.documentClient.send(command)
+
+    return response
+  }
+
+  async batchDelete(params) {
+    const command = new BatchWriteCommand(params)
+
+    const response = await this.documentClient.send(command)
+
+    return response
+  }
+
+  async queryByField(tableName, { field, value }) {
+    console.log(
+      `Getting items from DynamoDB table: ${tableName} by with ${field}=${value} `,
+    )
+    const params = {
+      TableName: tableName,
+      KeyConditionExpression: '#field = :value',
+      ExpressionAttributeNames: {
+        '#field': field,
+      },
+      ExpressionAttributeValues: {
+        ':value': value,
+      },
+    }
+    const response = await this.query(params)
+    console.log('Items retrieved successfully')
+    return response
   }
 
   async queryIndexByField(
@@ -112,7 +215,7 @@ module.exports = class DynamoDbAdapter {
       `Getting items from DynamoDB table: ${tableName} by with ${field}=${value} `,
     )
 
-    const ONE_HOUR = 1
+    const FIVE_MINUTES = 300000
     const expression = pastBookings
       ? '#field = :value'
       : '#field = :value AND #field2 >= :value2'
@@ -132,7 +235,9 @@ module.exports = class DynamoDbAdapter {
         }
       : {
           ':value': value,
-          ':value2': new Date(new Date().getTime() + ONE_HOUR).toISOString(),
+          ':value2': new Date(
+            new Date().getTime() + FIVE_MINUTES,
+          ).toISOString(),
         }
 
     const params = {
@@ -144,9 +249,19 @@ module.exports = class DynamoDbAdapter {
       ScanIndexForward: true,
       Limit: limit,
     }
+
+    const response = await this.query(params)
+    console.log('Items retrieved successfully')
+    return response
+  }
+
+  async query(params) {
     const command = new QueryCommand({
       ...params,
     })
-    return this.documentClient.send(command)
+
+    const response = await this.documentClient.send(command)
+
+    return response
   }
 }
--- src/lambdas/deleteItem/function.js
+++ src/lambdas/deleteItem/function.js
@@ -6,15 +6,14 @@ const EntityService = require('../../common/service/entityService')
 const handler = async (event, context) => {
   console.log(`Starting Lambda function ${context.functionName}`)
 
-  const id = event.pathParameters.id
-  const userId = event.pathParameters.sort
-  console.log(event.pathParameters)
+  const [id, userId] = event.pathParameters.id.split('-')
+
   const myEntityService = new EntityService()
 
-  const response = await myEntityService.get(id, userId)
+  const response = await myEntityService.delete(id, userId)
 
   return {
-    statusCode: 200,
+    statusCode: 203,
     'Content-type': 'application/json; charset=UTF-8',
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Methods': 'GET',
