4aff3d2bbd72e6afcf11b28ba892abd84905eb15
Rocamain <fjrocavazquez@gmail.com>
2023-05-12 16:31:44 +0100

    changes i global setup

--- __tests__/global_setup.js
+++ __tests__/global_setup.js
@@ -2,7 +2,7 @@ const { setup } = require('jest-dev-server')
 
 module.exports = async function globalSetup() {
   globalThis.servers = await setup({
-    command: `npx kill-port 3000 && npx kill-port 8000 &&sls export-env --all && npm run offline`,
+    command: `sls export-env --all && npm run offline`,
     launchTimeout: 50000,
     port: 3000,
   })
