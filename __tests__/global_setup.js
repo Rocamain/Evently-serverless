aeb29b1018309a9c49936d1146c7f151d1b2df20
Rocamain <fjrocavazquez@gmail.com>
2023-05-14 13:01:52 +0100

    Changes in serveless yml

--- __tests__/global_setup.js
+++ __tests__/global_setup.js
@@ -2,7 +2,7 @@ const { setup } = require('jest-dev-server')
 
 module.exports = async function globalSetup() {
   globalThis.servers = await setup({
-    command: `sls export-env --all && npm run offline`,
+    command: `npm run offline`,
     launchTimeout: 50000,
     port: 3000,
   })
