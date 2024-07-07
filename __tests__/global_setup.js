cb5be705b93cd21f7380ae72154e2d3d074137e0
Rocamain <fjrocavazquez@gmail.com>
2024-07-07 17:52:28 +0100

    Modifications for git action

--- __tests__/global_setup.js
+++ __tests__/global_setup.js
@@ -3,7 +3,7 @@ const { setup } = require('jest-dev-server')
 
 module.exports = async function globalSetup() {
   globalThis.servers = await setup({
-    command: `kill-port 4000 && kill-port 8000 && npm run offline`,
+    command: `kill-port 4000 && kill-port 8000 && npm run offline --httpPort=4000`,
     launchTimeout: 100000,
     port: 4000,
   })
