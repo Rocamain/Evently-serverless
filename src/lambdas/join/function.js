aef488040961db17738a68e0fb45815add33296e
Rocamain <fjrocavazquez@gmail.com>
2024-10-31 00:27:54 +0000

    Fixes Save profile picture and Save event pictures and remove comments

--- src/lambdas/join/function.js
+++ src/lambdas/join/function.js
@@ -6,7 +6,7 @@ const multipartBodyParser = require('@middy/http-multipart-body-parser')
 const handler = async (event, context) => {
   console.log(`Starting Lambda function ${context.functionName}`)
 
-  const { email, password, name, surname } = event.body
+  const { email, password, name, surname, profile_picture } = event.body
 
   const cognitoService = new CognitoService()
   await cognitoService.createNewUser({
@@ -14,10 +14,9 @@ const handler = async (event, context) => {
     password,
     name,
     surname,
-    file: event.body['profile picture'],
+    file: profile_picture,
   })
 
-  console.log(`${context.functionName} function complete`)
   return {
     statusCode: 201,
     headers: {
